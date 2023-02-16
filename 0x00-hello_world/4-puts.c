858711be795f01d2ab4d74adcd55540032826c32
karintoks <karintoks@gmail.com>
2023-02-16 08:42:31 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 
 /*
- * main-prints
- * Write a C program that prints exactly
- * "Programming is like building a multilingual puzzle,
- *  followed by a new line.
- *   Return: Always 0
+ * main - prints "Write a C program that prints exactly
+ * Programming is like building a multilingual puzzle,
+ * followed by a new line.
+ * Return: Always 0"
  */
 
 int main(void)
