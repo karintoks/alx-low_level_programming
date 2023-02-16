26b764a32303d0d4e7aa09814186e243b128e3a8
karintoks <karintoks@gmail.com>
2023-02-16 08:28:50 -0800

    mata

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
-/*Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line. Return: Always 0*/
-int main(void)/*this is a main function*/
+
+/*
+ * main-prints Write a C program that prints exactly
+ * "Programming is like building a multilingual puzzle,
+ *  followed by a new line.
+ *   Return: Always 0 */
+
+int main(void)
 	{
 		puts("\"Programming is like building a multilingual puzzle");
-		return (0);/*return: always 0*/
+		return (0);
 	}
