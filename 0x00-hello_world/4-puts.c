efe332fa6e218b37dbdc80d66b180cb76e223197
karintoks <karintoks@gmail.com>
2023-02-16 07:30:51 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /*Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.*/
 int main(void){
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
