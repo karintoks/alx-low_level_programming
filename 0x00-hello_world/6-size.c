21dc273b256d32e89657287c6a6a83e071566b7a
karintoks <karintoks@gmail.com>
2023-02-16 11:12:56 -0800

    smart

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,7 @@
 int main(void)
 	{
 		printf("Size of char: %zu byte(s)\n", sizeof(char));
+
 		printf("size of int; %zu byte(s)\n", sizeof(int));
 		printf("size of long int; %zu byte(s)\n", sizeof(long int));
 		printf("size of long long int; %zu byte(s)\n", sizeof(long long int));
