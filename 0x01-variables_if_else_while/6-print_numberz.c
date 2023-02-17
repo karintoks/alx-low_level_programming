bc1d4bae458d89b4ccccbb9e7dceeeaf9702276e
karintoks <karintoks@gmail.com>
2023-02-17 15:52:16 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Style
+*Return: 0 success
+*/
+int main(void)
+{
+int d;
+for (d = 0; d <= 0; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
