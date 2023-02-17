f98f05011b1c6089642556b973596e7b9503507f
karintoks <karintoks@gmail.com>
2023-02-17 15:40:35 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Style
+*Return: 0 success
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}
