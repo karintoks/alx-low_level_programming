a531df77fa8b0575fe0e377db694e4615a4edcbc
karintoks <karintoks@gmail.com>
2023-02-17 14:36:50 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,26 @@
-#include <stdio.h>                                                                      /**                                                                                      * main - Entry point                                                                    *                                                                                       * Return: Always 0                                                                      */                                                                                                                                                                             int main(void)                                                                          {                                                                                               char c;                                                                                                                                                                         char d;                                                                                                                                                                         c = 'a';                                                                                d = 'A';                                                                                while                                                                                           (c <= 'z')                                                                              {                                                                                               putchar(c);                                                                             c++;                                                                            }                                                                               while                                                                                           (d <= 'Z')                                                                              {                                                                                               putchar(d);                                                                             d++;                                                                            }                                                                               putchar('\n');                                                                          return (0);                                                                     }
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	char c;
+	char d;
+	c = 'a';
+	c = 'a';
+	while(c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while(d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
+			
