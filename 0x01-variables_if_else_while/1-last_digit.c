05f9b20a1af551d468d248319cf742511a557906
karintoks <karintoks@gmail.com>
2023-02-17 12:42:32 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,15 +13,15 @@ n = rand() - RAND_MAX / 2;
 lastsd = n % 10;
 if (lastsd > 5)
 {
-printf("last digit of %d is %d and is greater than 5\n", n, lastsd);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastsd);
 }
 else if (lastsd == 0)
 {
-printf("last digit of %d is %d and is 0\n", n, lastsd);
+printf("Last digit of %d is %d and is 0\n", n, lastsd);
 }
 else if (lastsd < 6 && lastsd != 0)
 {
-printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastsd);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastsd);
 }
 return (0);
 }
