f3acfb89233f331f5f258bbd7e5c85ecde4c87d2
karintoks <karintoks@gmail.com>
2023-02-16 16:38:57 -0800

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
 *main - Entry
-*Return: 1
+*Return: 1 (success)
 */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
