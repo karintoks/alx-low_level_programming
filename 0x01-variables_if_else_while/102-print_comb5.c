23b1a6a5e47220d1d0e03a58eb37614c89ec6037
karintoks <karintoks@gmail.com>
2023-02-17 18:16:44 -0800

    fine

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int p, q;
-for (p = '0'; p <= 98; p++)
+for (p = 0; p <= 98; p++)
 {
 for (q = p + 1; q <= 99; q++)
 {
@@ -16,7 +16,7 @@ putchar((p / 10) + '0');
 putchar((p % 10) + '0');
 putchar(' ');
 putchar((q / 10) + '0');
-putchar((p % 10) + '0');
+putchar((q % 10) + '0');
 if (p == 98 && q == 99)
 continue;
 putchar(',');
