aeb72f9fe59e4932a114a8321a1171c279ddd262
karintoks <karintoks@gmail.com>
2023-02-17 16:35:59 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Style
+*Return: 0 success
+*/
+int main(void)
+{
+int d;
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
