eb584158b37732c93aeece372d959fdabb83630f
karintoks <karintoks@gmail.com>
2023-02-17 16:07:57 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Style
+*Return: 0 success
+*/
+int main(void)
+{
+char low;
+for (low = 'z'; low >= 'a'; low--)
+putchar(low);
+putchar('\n');
+return (0);
+}
