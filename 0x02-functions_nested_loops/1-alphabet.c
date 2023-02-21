0886e1fee9c29ecdc83a1b3b1092d0fc3c65164a
karintoks <karintoks@gmail.com>
2023-02-21 13:26:28 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int c;
+for (c = 97; c <= 122; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
