b5792b5e7e654894294ba7aec8a7fb80f9cf63dc
karintoks <karintoks@gmail.com>
2023-02-22 10:38:12 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,9 +9,13 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
+
 if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+
 {
+
 if (month >= 2 && day >= 60)
+
 {
 day++;
 }
@@ -20,7 +24,9 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
+
 if (month == 2 && day == 60)
+
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
