70346c7ee41e6976bd837659c49cdcb233f5a6a2
karintoks <karintoks@gmail.com>
2023-02-22 04:05:37 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,21 +9,22 @@
  */
 
 int largest_number(int a, int b, int c)
+
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+	if (a > b && b > c)
+	{
+	largest = a;
+	}
+	else if (b > a && a > c)
+	{
+	largest = b;
+	}
+	else
+	{
+	largest = c;
+	}
 
-    return (largest);
+	return (largest);
 }
