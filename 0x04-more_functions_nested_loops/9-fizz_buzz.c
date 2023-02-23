fc116deca4b4c0f9332a898ae9cecad8c841b832
karintoks <karintoks@gmail.com>
2023-02-23 03:06:54 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entrypoint
+ *
+ * Description: prints FizzBuzz
+ *
+ * Return: void.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; ++i)
+	{
+		if ((i % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		
+		if (i < 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
