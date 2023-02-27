0ce3675d4f17595b27ae1d4d4fd0bee2920410e5
karintoks <karintoks@gmail.com>
2023-02-27 02:41:43 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,6 +5,7 @@
  * _isupper - checks for uppercase character
  * @c: the character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
+ * this code is written by karintoks alx cohort 12
  */
 int _isupper(int c)
 {
