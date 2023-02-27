ea5d3479ffd220906a5309f0f8748a845c947fbb
karintoks <karintoks@gmail.com>
2023-02-27 02:45:20 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  *reset_to_98 - resets value in address referenced by pointer.
- *   *@n: pointer.
- *
- *     *Return: void.
+ * reset_to_98 - resets value in address referenced by pointer.
+ * @n: pointer.
+ * Return: void.
+ * This code is written by Karintoks ALX cohort 12
  */
 void reset_to_98(int *n)
 {
