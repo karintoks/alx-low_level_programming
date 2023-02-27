51d259c46710e37ddab0c9104ffa61bb339b39d0
karintoks <karintoks@gmail.com>
2023-02-27 02:47:17 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,8 +3,8 @@
  *swap_int - swap value of two ints.
  *@a: pointer to one int.
  *@b: pointer to second int.
- *
  *Return: void.
+ *This code is written by Karintoks.
  */
 void swap_int(int *a, int *b)
 {
