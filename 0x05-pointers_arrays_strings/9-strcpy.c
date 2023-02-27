1a57dea5c29d216bce53005068e5dc690a38c5ff
karintoks <karintoks@gmail.com>
2023-02-27 04:51:53 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,8 +3,8 @@
  * _strcpy - copy stringat the end of another string including \0.
  * @dest: pointer to string to be copied to.
  * @src: pointer to string to be copied.
- *
  * Return: pointer to dest.
+ * This code is written by Karintoks.
  */
 char *_strcpy(char *dest, char *src)
 {
