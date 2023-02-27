14e6aaf26005f79e2bf751e641a3e1e4a45ef7b7
karintoks <karintoks@gmail.com>
2023-02-27 04:49:33 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,8 +4,8 @@
  * print_array - prints n elements of an array of integers.
  * @a: pointer to array.
  * @n: number of array elements.
- *
  * Return: void.
+ * This code is written by Karintoks.
  */
 void print_array(int *a, int n)
 {
