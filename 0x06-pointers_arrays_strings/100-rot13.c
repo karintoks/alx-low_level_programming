1d6d1cf7852a8fed10d652e2950d688515850866
karintoks <karintoks@gmail.com>
2023-02-28 02:19:18 -0800

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encode strings using rot13 encoding.
+ * @str: string to be encoded
+ * Return: encoded string.
+ * This code is written by karintoks.
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char arr1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char arr2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		for (j = 0; j < 52; ++j)
+		{
+			if (str[i] == arr1[j])
+			{
+				str[i] = arr2[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
