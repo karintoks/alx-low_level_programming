b8bdf93485cce28ceb37ec57e9240a9806924699
karintoks <karintoks@gmail.com>
2023-03-01 10:23:20 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,12 @@
-#i#include <stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
   int n;
-  int b[5];
+  int a[5];
   int *p;
 
-  b[2] = 1024;
+  a[2] = 1024;
   p = &n;
   /*
    * write your line of code here...
@@ -16,8 +16,8 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 5) = 98;
+  *(p+5) = 98;
   /* ...so that this prints 98\n */
-  printf("b[2] = %d\n", b[2]);
+  printf("a[2] = %d\n", a[2]);
   return (0);
 }
