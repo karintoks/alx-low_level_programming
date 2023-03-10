f96860483df3ef79b2fe4a52640bf04eecb75e9c
karintoks <karintoks@gmail.com>
2023-03-10 12:13:18 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - prints minimum number of coins
- * Return: 0
- *
+ * main - program prints minimum number of coins to
+ * make change
+ * @argc: size of @argv
+ * @argv: contain the name of program and exactly one more
+ * argument;
+ * Return: 1 if the amount s of arguments passed is not exactly 1,
+ * else 0
+ * This code is written by karintoks
  */
-int main(void)
+int main(int argc, char **argv)
 {
-int cents;
-cents = atoi(argv[1]);
-printf("%d", min(cents, 0));
-return (0);
+	int cents;
+	int coins = 0;
+	int cv[5] = {25, 10, 5, 2, 1};   /*coin values*/
+	int i;
+
+	if (argc !=  2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+	if (cents > 0)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			coins += cents / cv[i];
+			cents -= (cents / cv[i]) * cv[i];
+		}
+	}
+
+	printf("%d\n", coins);
+	return (0);
 }
