0ea90bba1ca0b3d3d14e6cad39fa2606b4c29898
karintoks <karintoks@gmail.com>
2023-04-03 13:20:39 -0700

    C file commit by DONJOR

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t num = 0;
+while (h)
+{
+printf("%d\n", h->n);
+num=num+1;
+h = h->next;
+}
+return (num);
+}
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ * written by DONJOR
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num=num+1;
+		h = h->next;
+	}
+
+	return (num);
+}
