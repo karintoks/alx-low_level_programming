3527ac736941359d97137d2c560b14b86a520c6e
karintoks <karintoks@gmail.com>
2023-04-03 13:32:56 -0700

    C files commit by DONJOR

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (node = 0; node < (idx - 1); node=node+1)
+	for (node = 0; node < (idx - 1); node = node + 1)
 	{
 		if (copy == NULL || copy->next == NULL)
 			return (NULL);
