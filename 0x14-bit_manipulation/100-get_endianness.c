f127e420c6d8ff3b41ac4b15b6dd88256622fb2f
karintoks <karintoks@gmail.com>
2023-04-07 05:05:33 -0700

    C files commit by DONJOR

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,9 +1,9 @@
 #include"main.h"
 
 /**
- * This code is written by DONJOR
  * get_endianness - checks if a machine is little or big endian
  * Return: 0 for big, 1 for little
+ * This code is written by DONJOR
  */
 int get_endianness(void)
 {
